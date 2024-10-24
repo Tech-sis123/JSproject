481acd123e978fcc18459eb0dd3b78ee119637ff
Tech-sis123 <esabublessing@gmail.com>
2024-10-24 16:30:59 +0400

    app2 commit

---
+++ app.js
@@ -0,0 +1,101 @@
+const nigga = "policemonkey"
+console.log(nigga);
+const hello = "Hello, world";
+console.log(hello);
+let message = "Hi there, i'm running my first JS code";
+console.log(message)
+let integernum = 10;
+let floatnum = 3.14;
+console.log(integernum, floatnum);
+let animal = {
+    name: 'Dog',
+    age: 1.5,
+};
+console.log(animal);
+var num = 6;
+console.log(num);
+let b = 3 + 7
+console.log(b);
+let d = "hello"+ " "+ "world";
+console.log(d);
+let con = 5
+con++
+console.log(con);
+var product = 5 * 8;
+console.log(product);
+let rem = 12 % 7;
+console.log(rem);
+let isEqual = 5 === '5';
+console.log(isEqual);
+let x = 12;
+let y = 14;
+let And = x && y;
+console.log(And);
+let Or = x // y
+console.log(Or);
+let Not = !x
+let Not2 = !y
+console.log(Not, Not2);
+let age  = 25
+let correct = (age<=60) ? "Allowed" : "Not allowed";
+console.log(correct);
+let V = 12;
+V += 14;
+console.log(V);
+let Age = 50
+if (Age<= 35) {
+    console.log("Yay");
+
+    
+} else {
+    console.log("Snap");
+    
+}
+let day = "Thursday"
+switch (day) {
+    case 'Monday': 
+    console.log("Start of the Week");
+    break;
+    case "Tuesday":
+        console.log("Second day of the week");
+        break ;
+        case "Wednesday":
+        console.log("Middle of the week");
+        break;
+        default:
+        console.log("Another day of the week");
+            
+}
+
+let title = "PROPERTIES OF A CAR FOR SALE"
+console.log(title);
+
+const carname = "Mercedes Benz"
+console.log("Name of car:", carname);
+
+const carcolour = "Grey"
+console.log("Colour of car:", carcolour);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
