d018311183963b18c4b45e80423a21ca3af820c4
Tech-sis123 <esabublessing@gmail.com>
2024-10-24 18:53:50 +0400

    app2.js

--- app.js
+++ app.js
@@ -1,71 +1,8 @@
-const nigga = "policemonkey"
-console.log(nigga);
+
 const hello = "Hello, world";
 console.log(hello);
 let message = "Hi there, i'm running my first JS code";
 console.log(message)
-let integernum = 10;
-let floatnum = 3.14;
-console.log(integernum, floatnum);
-let animal = {
-    name: 'Dog',
-    age: 1.5,
-};
-console.log(animal);
-var num = 6;
-console.log(num);
-let b = 3 + 7
-console.log(b);
-let d = "hello"+ " "+ "world";
-console.log(d);
-let con = 5
-con++
-console.log(con);
-var product = 5 * 8;
-console.log(product);
-let rem = 12 % 7;
-console.log(rem);
-let isEqual = 5 === '5';
-console.log(isEqual);
-let x = 12;
-let y = 14;
-let And = x && y;
-console.log(And);
-let Or = x // y
-console.log(Or);
-let Not = !x
-let Not2 = !y
-console.log(Not, Not2);
-let age  = 25
-let correct = (age<=60) ? "Allowed" : "Not allowed";
-console.log(correct);
-let V = 12;
-V += 14;
-console.log(V);
-let Age = 50
-if (Age<= 35) {
-    console.log("Yay");
-
-    
-} else {
-    console.log("Snap");
-    
-}
-let day = "Thursday"
-switch (day) {
-    case 'Monday': 
-    console.log("Start of the Week");
-    break;
-    case "Tuesday":
-        console.log("Second day of the week");
-        break ;
-        case "Wednesday":
-        console.log("Middle of the week");
-        break;
-        default:
-        console.log("Another day of the week");
-            
-}
 
 let title = "PROPERTIES OF A CAR FOR SALE"
 console.log(title);
@@ -74,7 +11,52 @@ const carname = "Mercedes Benz"
 console.log("Name of car:", carname);
 
 const carcolour = "Grey"
-console.log("Colour of car:", carcolour);
+console.log("Colour:", carcolour);
+
+const carmodel = "SUV Gclass"
+console.log("Car model:", carmodel);
+
+const carengine = "Hybrid"
+console.log("Engine Type:", carengine);
+
+const cartrans = "Automatic"
+console.log("Transmission:", cartrans);
+
+const carbody = "Wagon"
+console.log("Body Style:", carbody);
+
+const carfuel = "Diesel"
+console.log("Fuel Type:", carfuel);
+
+const carseat = "6 seats"
+console.log("Seating Capacity:", carseat);
+
+const carperform = "AMG"
+console.log("Performance Division:", carperform);
+
+// CAR
+let car = {
+    Nameofcar: "Mercedes Benz",
+    Colour: "Grey",
+    Model : "SUV Gclass",
+    Engine: "Hybrid",
+    Transmission: "Automatic",
+    Bodystyle: "Wagon",
+    Fueltype: "Diesel",
+    Seatingcapacity: "6 seats",
+    Performance: "AMG",
+
+}
+console.log(car);
+
+
+
+
+
+
+
+
+
 
 
 
